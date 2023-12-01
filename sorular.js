10e04e5b3ecbe508c5a9433b4199616ac0356d15
Tristy <129403999+TheTristyXD62@users.noreply.github.com>
2023-12-01 14:54:32 +0300

    Add files via upload

---
+++ sorular.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    //Kısa Sorular\\
+
+    soru1: "İsminiz Nedir?",
+    örnekcevap1: "İsminiz",
+
+    soru2: "Kaç Yaşındasınız?",
+    örnekcevap2: "Yaşınız",
+
+    soru3: "Discord'daki Aktiflik Süreniz Nedir?",
+    örnekcevap3: "Aktiflik Süreniz",
+
+    soru4: "Yetkili Olmanızın Amacı Ne?",
+    örnekcevap4: "Yetkili Olma Amacınız",
+
+    soru5: "Herhangi Bir Sunucuda Adminlik Yaptın mı?",
+    örnekcevap5: "Yaptım / Yapmadım",
+
+    //Paragraf burdan sonrası\\
+
+    soru6: "Neden Bizi Tercih Ediyorsunuz",
+    örnekcevap6: "Tercih Etme Nedeniniz",
+
+    soru7: "Chatta Biri Sana Küfür Ediyor Ne Yaparsın",
+    örnekcevap7: "Ne Yaparsınız ?",
+
+}
